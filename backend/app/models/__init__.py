5621a7f7830175387dbb21544043473b4addd86c
Chris Anderson <andersonc@microsoft.com>
2025-05-12 01:47:04 +0000

    add frontend and backend

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+from .item import *
+from .user import *
+from .message import *
+from .auth import *
---
+++ backend/app/models/item.py
@@ -0,0 +1,42 @@
+from typing import Sequence
+import uuid
+
+from .user import User
+from pydantic import EmailStr
+from sqlmodel import Field, Relationship, SQLModel
+
+# Shared properties
+class ItemBase(SQLModel):
+    id: uuid.UUID
+    owner_id: uuid.UUID
+    title: str = Field(min_length=1, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+
+
+# Properties to receive on item creation
+class ItemCreate(ItemBase):
+    pass
+
+
+# Properties to receive on item update
+class ItemUpdate(ItemBase):
+    title: str | None = Field(default=None, min_length=1, max_length=255)  # type: ignore
+
+
+# Database model, database table inferred from class name
+class Item(ItemBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    owner_id: uuid.UUID = Field(
+        foreign_key="user.id", nullable=False, ondelete="CASCADE"
+    )
+    owner: User | None = Relationship(back_populates="items")
+
+
+# Properties to return via API, id is always required
+class ItemPublic(ItemBase):
+    pass
+
+
+class ItemsPublic(SQLModel):
+    data: Sequence[ItemBase]
+    count: int
